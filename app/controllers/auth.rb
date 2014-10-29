f288b6315f71b4f8ef3b676daf27d76d2c6c8d4c
Matthew Bunday <zenceph@gmail.com>
2014-10-29 14:22:51 -0400

    Working on auth flow.

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,11 +1,22 @@
 get '/' do
-  erb :welcome
+  redirect to '/subs'
 end
 
 get '/login' do
-  
+  erb :'auth/login'
 end
 
 get '/signup' do
-  
+  erb :'auth/signup'
+end
+
+post '/signup' do
+  user = User.new(params[:user])
+
+  if user.save
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    # display an error message and have them try again
+  end
 end
