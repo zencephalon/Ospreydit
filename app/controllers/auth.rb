df91079ff767456e70be0394d3e679081e54b148
Matthew Bunday <zenceph@gmail.com>
2014-10-29 14:35:35 -0400

    Finished login.

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -6,8 +6,24 @@ get '/login' do
   erb :'auth/login'
 end
 
+post '/login' do
+  user = User.find_by(name: params[:name]).try(:authenticate, params[:password])
+
+  if user
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    redirect to ('/login?error=Username or password incorrect')
+  end
+end
+
 get '/signup' do
   erb :'auth/signup'
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
 end
 
 post '/signup' do
