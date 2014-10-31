e151e45222eb0b149bd64c6ca87e98d11567306d
Matthew Bunday <zenceph@gmail.com>
2014-10-31 14:56:57 -0400

    Ajax delete functionality.

--- public/js/application.js
+++ public/js/application.js
@@ -3,5 +3,18 @@ $(document).ready(function() {
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('.delete_btn').click(function(event) {
+    event.preventDefault();
+    $target = $(event.target);
+    $target.val("Deleting...");
+    $target.attr("disabled", "true");
+    action = $target.parent().attr('action');
+
+    $.ajax({
+      url: action,
+      type: "DELETE"
+    }).done(function() {
+      $target.parents('.comment').remove();
+    });
+  });
 });
