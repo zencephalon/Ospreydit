69452066dfafe28f24fb07f8be2879df4cbe9d5b
Matthew Bunday <zenceph@gmail.com>
2014-11-03 15:42:44 -0500

    Implement replying to comments with AJAX.

--- public/js/application.js
+++ public/js/application.js
@@ -3,6 +3,26 @@ $(document).ready(function() {
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
+  $('.reply_link').click(function(event) {
+    event.preventDefault();
+    $(event.target).parent().children('.reply_box').show();
+    $(event.target).remove();
+  });
+
+  $('.comment_form').submit(function(event) {
+    event.preventDefault();
+    console.log(event.target);
+    $form = $(event.target);
+    $.ajax({
+      type: $form.attr('method'),
+      url: $form.attr('action'),
+      data: $form.serialize()
+    }).done(function(response) {
+      $form.closest('.comment').children('.children').append("<li>" + response + "</li>")
+      $form.parent().remove();
+    });
+  })
+
   $('.delete_btn').click(function(event) {
     event.preventDefault();
     $target = $(event.target);
